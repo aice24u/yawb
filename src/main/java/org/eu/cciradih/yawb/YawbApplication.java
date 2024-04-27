1370bb2d5f255144edcc91d42f53406563ddbadb
Ai_xu <3085889216@qq.com>
2024-04-27 20:53:39 +0800

    test

--- src/main/java/org/eu/cciradih/yawb/YawbApplication.java
+++ src/main/java/org/eu/cciradih/yawb/YawbApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YawbApplication {
     public static void main(String[] args) {
+        System.out.println("你好");
         SpringApplication.run(YawbApplication.class, args);
     }
 }
